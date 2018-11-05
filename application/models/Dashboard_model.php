b692c87d6ffbd9fa099e1708731f24dae38772de
Darlan Nakamura <darlannakamura@hotmail.com>
2018-11-05 13:26:18 -0200

    Corrigido Gráfico de hoje
    1 - O gráfico mostrava as ordens finalizadas e as ordens criadas no dia de hoje. É interessante mostrar apenas as criadas hoje.

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -59,17 +59,36 @@ class Dashboard_model extends CI_Model {
         }
     }
 
-    public function get_ordens_hoje() {
-        //         SELECT ordem_servico_fk from historicos_ordens WHERE DAY(historico_ordem_tempo) = DAY(CURDATE()) 
-        // AND situacao_fk = 2;
-        $this->db->select('count(ordem_servico_fk) as count');
-        $this->db->from('historicos_ordens');
-        $this->db->where('DAY(historico_ordem_tempo) = DAY(CURDATE())');
-        $this->db->where('situacao_fk', ID_SITUACAO_ABERTA);
+    // public function get_ordens_hoje() {
+    //     //         SELECT ordem_servico_fk from historicos_ordens WHERE DAY(historico_ordem_tempo) = DAY(CURDATE()) 
+    //     // AND situacao_fk = 2;
+    //     $this->db->select('count(ordem_servico_fk) as count');
+    //     $this->db->from('historicos_ordens');
+    //     $this->db->where('DAY(historico_ordem_tempo) = DAY(CURDATE())');
+    //     $this->db->where('situacao_fk', ID_SITUACAO_ABERTA);
 
-        $result = $this->db->get()->result();
+    //     $result = $this->db->get()->result();
+    //     if ($result) {
+    //         return ($result);
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+    public function get_ordens_hoje(){
+        // $this->db->query('SELECT count(*) as quantidade FROM (select ordem_servico_fk, historico_ordem_pk,
+        //     min(historico_ordem_tempo) as data_criacao 
+        //     from evidencia.historicos_ordens 
+        //     group by ordem_servico_fk) as t WHERE DAY(t.data_criacao) = DAY(curdate());');
+        $this->db->select('count(*) as quantidade');
+        $this->db->from('(select ordem_servico_fk, historico_ordem_pk,
+            min(historico_ordem_tempo) as data_criacao 
+            from evidencia.historicos_ordens 
+            group by ordem_servico_fk) as t');
+        $this->db->where('DAY(t.data_criacao) = DAY(curdate())');
+        $result = $this->db->get()->row();
         if ($result) {
-            return ($result);
+            return ($result->quantidade);
         } else {
             return false;
         }
