b7bccbdda7daee55162b41334178db629626cc00
Darlan Nakamura <darlannakamura@hotmail.com>
2018-11-06 14:40:53 -0200

    Agora vai

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://http://evidencia.azurewebsites.net';
+$config['base_url'] = 'http://evidencia.azurewebsites.net';
 
 /*
 |--------------------------------------------------------------------------
