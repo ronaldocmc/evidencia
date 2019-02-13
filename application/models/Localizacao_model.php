156bae95da5bf7ff5cd52168621e281ac827057e
= <darlannakamura@hotmail.com>
2019-02-13 10:23:12 -0200

    Adição do MY_Model e Generic model com implementação no organizacao

---
+++ application/models/Localizacao_model.php
@@ -0,0 +1,47 @@
+<?php
+require_once APPPATH."core\MY_Model.php";
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Localizacao_model extends MY_Model {
+    const NAME = 'localização';
+	const TABLE_NAME = 'localizacoes';
+    const PRI_INDEX = 'localizacao_pk';
+    
+    const FORM = array(
+        'localizacao_rua',
+        'localizacao_num',
+        'localizacao_bairro',
+        'localizacao_municipio'
+    );
+
+    public function config_form_validation()
+    {
+        $this->CI->form_validation->set_rules(
+            'localizacao_rua',
+            'Logradouro',
+            'trim|required|max_length[128]'
+        );
+
+        $this->CI->form_validation->set_rules(
+            'localizacao_num',
+            'número',
+            'trim|required|max_length[10]'
+        );
+
+        $this->CI->form_validation->set_rules(
+            'localizacao_bairro',
+            'bairro',
+            'trim|required|max_length[128]'
+        );
+
+        $this->CI->form_validation->set_rules(
+            'localizacao_municipio',
+            'município',
+            'trim|required|max_length[128]'
+        );
+    }
+}
+?>
