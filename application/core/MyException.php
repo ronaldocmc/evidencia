747e140c9a557485f91376da85b31fd927b7bf8b
Gustavo Gimenez de Deus <gustavogdd2@gmail.com>
2019-02-26 15:29:39 -0300

    Edição básica de arquivo

--- application/core/MyException.php
+++ application/core/MyException.php
@@ -2,16 +2,13 @@
 require_once dirname(__FILE__) . "/../controllers/Response.php";
 class MyException extends Exception {
 	public $code;
-	// public function __construct()
-	// {
-	// 	parent::__construct($message);
-	// 	$this->code = Response::BAD_REQUEST;
-	// }
+
 	public function __construct($message, $code)
 	{
 		parent::__construct($message);
 		$this->code = $code;
 	}	
+	
 	public function set_code($message, $code){
 		$this->code = $code;
 	}
