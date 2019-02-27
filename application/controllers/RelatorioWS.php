5db74097e1b25fbcce4cff3c12fc7c2527c269f2
Pietro Barcarollo Schiavinato <pietrobschiavinato@outlook.com>
2019-02-27 12:08:11 -0300

    adicionado a função getImages no model

--- application/controllers/RelatorioWS.php
+++ application/controllers/RelatorioWS.php
@@ -22,7 +22,6 @@ class RelatorioWS extends MY_Controller
      * @var Response
      */
     private $response;
-    private $relatorio;
 
     /**
      * Construtor da classe, responsável por setar a timezone
@@ -50,77 +49,112 @@ class RelatorioWS extends MY_Controller
     }
 
     /**
-     * 
+     *
      * Método responsável por retornar as ordens de serviço do relatório (do dia) da qual o funcionário está vinculado.
      *
      * @param Objeto JSON com os dados para atualizacao
      */
     public function get()
     {
-        $this->load->helper('attempt');
-        $this->load->helper('token');
-        $this->load->model('tentativa_model');
-        $this->load->model('atualizacao_model');
-        $this->load->model('relatorio_model');
-        
-        $header_obj = apache_request_headers();
-            
-        $attempt_result = verify_attempt($this->input->ip_address());
+        $this->load->model('Relatorio_model', 'relatorio');
+        $this->load->helper('exception');
 
-        if ($attempt_result === true) {
-           
-            $token_decodificado = json_decode(token_decrypt($header_obj['Token']));
-            $id_funcionario = $token_decodificado->id_funcionario;
-            
-            //$this->update_relatorio($id_funcionario);
+        try {
 
-            //get relatorio
-            $ordens_servicos = $this->get_relatorio_do_funcionario($id_funcionario);
+            $obj = json_decode(file_get_contents('php://input'));
+            $headers = apache_request_headers();
+            $token_decodificado = json_decode(token_decrypt($headers['token']));
 
-            if($ordens_servicos)
-            {
-                $this->response->set_code(Response::SUCCESS);
-                $this->response->add_data('ordens_servicos', $ordens_servicos);
-            }
-            else
-            {
-                $this->response->set_code(Response::NOT_FOUND);
-                $this->response->set_message('Relatório não encontrado. Verifique se realmente existe um relatório em aberto para você.');
+            $where['relatorios.relatorio_func_responsavel'] = $token_decodificado->id_funcionario;
+            $where['relatorios.ativo'] = 1;
+
+            $relatorio = $this->relatorio->get_one(
+                '*',
+                $where,
+                -1,
+                -1
+            );
+
+            if ($relatorio == null){
+                throw new MyException('Nenhum relatório ativo foi encontrado!', Response::NOT_FOUND);
             }
 
-            
+            $ordens_servicos = $this->relatorio->get_all(
+                'ordens_servicos.ordem_servico_pk, ordens_servicos.ordem_servico_cod, ordens_servicos.ordem_servico_desc, ordens_servicos.ordem_servico_comentario, ordens_servicos.ordem_servico_criacao,
+                funcionarios.funcionario_nome, funcionarios.funcionario_caminho_foto,
+                localizacoes.localizacao_lat, localizacoes.localizacao_long, localizacoes.localizacao_rua, localizacoes.localizacao_num,
+                prioridades.prioridade_nome,
+                procedencias.procedencia_nome,
+                servicos.servico_nome',
+                $where,
+                -1,
+                -1,
+                [
+                    ['table' => 'relatorios_os', 'on' => 'relatorios_os.relatorio_fk = relatorios.relatorio_pk'],
+                    ['table' => 'ordens_servicos', 'on' => 'relatorios_os.os_fk = ordens_servicos.ordem_servico_pk'],
+                    ['table' => 'localizacoes', 'on' => 'ordens_servicos.localizacao_fk = localizacoes.localizacao_pk'],
+                    ['table' => 'funcionarios', 'on' => 'ordens_servicos.funcionario_fk = funcionarios.funcionario_pk'],
+                    ['table' => 'prioridades', 'on' => 'ordens_servicos.prioridade_fk = prioridades.prioridade_pk'],
+                    ['table' => 'procedencias', 'on' => 'ordens_servicos.procedencia_fk = procedencias.procedencia_pk'],
+                    ['table' => 'servicos', 'on' => 'ordens_servicos.servico_fk = servicos.servico_pk'],
+                ]
+            );
 
-        } else {
-            $this->response->set_code(Response::FORBIDDEN);
-            $this->response->set_message($attempt_result);
+            $imagens = null;
+            if ($ordens_servicos) {
+                $imagens = $this->relatorio->get_images($relatorio->relatorio_pk);
+
+                foreach ($ordens_servicos as $os) {
+                    $os->ordem_servico_criacao = date('d/m/Y H:i:s', strtotime($os->ordem_servico_criacao));
+                    $os->imagens = [];
+
+                    foreach ($imagens as $img) {
+                        if ($os->ordem_servico_pk == $img->ordem_servico_fk) {
+
+                            array_push($os->imagens, $img);
+
+                            unset($img);
+                        }
+                    }
+                }
+
+            }
+
+            $this->response->add_data("relatorio", $relatorio);
+            $this->response->add_data("ordens_servicos", $ordens_servicos);
+
+            $this->response->set_code(Response::SUCCESS);
+            $this->response->send();
+
+        } catch (MyException $e) {
+            handle_my_exception($e);
+        } catch (Exception $e) {
+            handle_exception($e);
         }
-
-        $this->response->send();
-        $this->__destruct();
     }
 
     /**
-    * FINALIZAR RELATÓRIO
-    **/
+     * FINALIZAR RELATÓRIO
+     **/
     public function put()
     {
         $this->load->helper('attempt');
         $this->load->helper('token');
         $this->load->model('tentativa_model');
         $this->load->model('atualizacao_model');
-        
+
         $header_obj = apache_request_headers();
-            
+
         $attempt_result = verify_attempt($this->input->ip_address());
 
         if ($attempt_result === true) {
-           
+
             $token_decodificado = json_decode(token_decrypt($header_obj['Token']));
             $id_funcionario = $token_decodificado->id_funcionario;
-            
-            $this->update_relatorio($id_funcionario); 
-            //se deu certo vai enviar o response dentro desse método  
-            $this->response->set_code(Response::SUCCESS);          
+
+            $this->update_relatorio($id_funcionario);
+            //se deu certo vai enviar o response dentro desse método
+            $this->response->set_code(Response::SUCCESS);
 
         } else {
             $this->response->set_code(Response::FORBIDDEN);
@@ -132,51 +166,45 @@ class RelatorioWS extends MY_Controller
     }
 
     /**
-    * Neste método, vamos receber os relatórios que estão em aberto do funcionário
-    * e verificar se o último histórico das ordens de serviço daquele relatório estão 
-    * diferente de "Em Andamento".
-    * Caso esteja, o status do relatório vai para 1, isto é, o relatório foi concluído.
-    * O status estar em 1 não significa que o relatório foi concluído com sucesso, 
-    * significa que não é mais o relatório corrente (em execução).
-    * Caso haja uma ordem com status "Em Andamento", vamos dar a mensagem que aquele 
-    * relatório ainda não foi concluído.
-    **/
-    public function update_relatorio($id_funcionario){
+     * Neste método, vamos receber os relatórios que estão em aberto do funcionário
+     * e verificar se o último histórico das ordens de serviço daquele relatório estão
+     * diferente de "Em Andamento".
+     * Caso esteja, o status do relatório vai para 1, isto é, o relatório foi concluído.
+     * O status estar em 1 não significa que o relatório foi concluído com sucesso,
+     * significa que não é mais o relatório corrente (em execução).
+     * Caso haja uma ordem com status "Em Andamento", vamos dar a mensagem que aquele
+     * relatório ainda não foi concluído.
+     **/
+    public function update_relatorio($id_funcionario)
+    {
         $this->load->model('relatorio_model');
         $this->load->model('historico_model');
 
         //Pegamos os relatórios em aberto:
         $relatorios_em_aberto = $this->relatorio_model->get_relatorios(['status' => 0, 'funcionario_fk' => $id_funcionario]);
 
-        if(count($relatorios_em_aberto) > 0)
-        {
+        if (count($relatorios_em_aberto) > 0) {
             date_default_timezone_set('America/Sao_Paulo');
 
             //Percorremos todos os relatórios
-            foreach($relatorios_em_aberto as $relatorio)
-            {
+            foreach ($relatorios_em_aberto as $relatorio) {
                 //pegando as ordens do relatório:
                 $ordens_relatorio = $this->get_ordens_relatorio($relatorio->relatorio_pk);
                 //se existir ordens:
-                if(count($ordens_relatorio) > 0)
-                {
+                if (count($ordens_relatorio) > 0) {
                     //percorremos as ordens:
-                    foreach($ordens_relatorio as $os)
-                    {
+                    foreach ($ordens_relatorio as $os) {
                         //pegamos o último histórico (IMPORTANTE: O ULTIMO HISTÓRICO É O ID DA SITUACAO!!!):
                         $os->ultimo_historico = $this->historico_model->get_last_historico($os->ordem_servico_pk);
-                       
-                        if($os->ultimo_historico == 2)
-                        { 
+
+                        if ($os->ultimo_historico == 2) {
                             //SE O STATUS FOR EM ANDAMENTO:
                             $this->response = new Response();
                             $this->response->set_code(Response::BAD_REQUEST);
                             $this->response->set_message('O relatório corrente ainda não foi concluído.');
                             $this->response->send();
                             die();
-                        }
-                        else
-                        {
+                        } else {
                             $this->relatorio_model->update_relatorios_os_verificada(
                                 ['os_fk' => $os->os_fk, 'os_verificada' => 0], 1
                             );
@@ -188,16 +216,14 @@ class RelatorioWS extends MY_Controller
                 $this->relatorio_model->update(['status' => 1], ['relatorio_pk' => $relatorio->relatorio_pk]);
 
                 $this->relatorio_model->set_data_entrega($relatorio->relatorio_pk);
-                
+
                 $this->response->set_code(Response::SUCCESS);
                 $this->response->set_message('Situação do relatório atualizado com sucesso.');
                 $this->response->send();
                 die();
             }
 
-        }
-        else 
-        {
+        } else {
             $this->response = new Response();
             $this->response->set_code(Response::NOT_FOUND);
             $this->response->set_message('Não encontramos nenhum relatório em andamento para você.');
@@ -206,19 +232,18 @@ class RelatorioWS extends MY_Controller
         }
     }
 
-
     /**
     Retorna o relatório que está em aberto do funcionário
-    **/
-    private function get_relatorio_do_funcionario($id_funcionario){
+     **/
+    private function get_relatorio_do_funcionario($id_funcionario)
+    {
         $this->load->model('relatorio_model');
 
         //precisamos descobrir o id do relatório:
         $relatorio = $this->relatorio_model->get_relatorio_do_funcionario($id_funcionario);
-        
+
         //se o relatório existir:
-        if($relatorio)
-        {
+        if ($relatorio) {
             //vamos pegar todas as ordens de serviços que pertence a este relatório:
 
             // $ordens_servicos = $this->relatorio_model->get(['relatorio_fk' => $relatorio->relatorio_pk]);
@@ -227,27 +252,24 @@ class RelatorioWS extends MY_Controller
             $this->relatorio_model->update(['pegou_no_celular' => 1], ['relatorio_pk' => $relatorio->relatorio_pk]);
 
             return $this->get_ordens_relatorio($relatorio->relatorio_pk);
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
-
-    private function get_ordens_relatorio($id_relatorio){
+    private function get_ordens_relatorio($id_relatorio)
+    {
         $this->load->model('relatorio_model');
         $this->load->model('Historico_model', 'historico_model');
 
         $ordens_servicos = $this->relatorio_model->get(['relatorio_fk' => $id_relatorio]);
 
-        foreach($ordens_servicos as $os){
+        foreach ($ordens_servicos as $os) {
             $id_ultimo_historico = $this->historico_model->get_id_last_historico($os->ordem_servico_pk);
             $os->imagem_situacao_caminho = $this->historico_model->get_imagem($id_ultimo_historico);
         }
 
         return $ordens_servicos;
     }
-
 
 }
