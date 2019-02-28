0999ca0a905f4166e6f02ccfde03da221bc14803
= <darlannakamura@hotmail.com>
2019-02-28 09:37:29 -0300

    alterações de dirname para APPPATH em require_once

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -2,8 +2,10 @@
     exit('No direct script access allowed');
 }
 
-require_once dirname(__FILE__) . "\Generic_Model.php";
-require_once dirname(__FILE__) . "\MyException.php";
+require_once APPPATH."core/Generic_Model.php";
+require_once APPPATH."core/MyException.php";
+//require_once dirname(__FILE__) . "/Generic_Model.php";
+// require_once dirname(__FILE__) . "/MyException.php";
 class MY_Model extends Generic_Model
 {
     public $object;
