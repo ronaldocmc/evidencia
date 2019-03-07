c4d93cfbf1f696147492072cde63f1eedbeccb7f
Pietro Barcarollo Schiavinato <pietrobschiavinato@outlook.com>
2019-03-07 08:44:25 -0300

    Faltou esse

--- application/controllers/AccessWS.php
+++ application/controllers/AccessWS.php
@@ -194,7 +194,7 @@ class AccessWS extends MY_Controller
             $obj = apache_request_headers();
 
 
-            $new_token = verify_token($obj['token'], $this->response);
+            $new_token = verify_token($obj['Token'], $this->response);
 
             if ($new_token) {
                 $dados['token'] = $new_token;
