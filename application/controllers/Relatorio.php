07527db9bf5cf9b0d05153361adfa56b9d701466
Gustavo Gimenez de Deus <gustavogdd2@gmail.com>
2019-03-13 08:44:10 -0300

    Apagada a função mapa duplicada

--- application/controllers/Relatorio.php
+++ application/controllers/Relatorio.php
@@ -27,95 +27,6 @@ class Relatorio extends CRUD_Controller
         $this->response = new Response();
     }
 
-    public function mapa()
-    {
-        $this->load->model('Prioridade_model', 'prioridade');
-        $this->load->model('Situacao_model', 'situacao');
-        $this->load->model('Departamento_model', 'departamento');
-        $this->load->model('Servico_model', 'servico');
-        $this->load->model('Tipo_Servico_model', 'tipo_servico');
-        $this->load->helper('form');
-        $prioridades = $this->prioridade->get_all(
-            '*',
-            ['organizacao_fk' => $this->session->user['id_organizacao']],
-            -1,
-            -1
-        );
-        $situacoes = $this->situacao->get_all(
-            '*',
-            ['organizacao_fk' => $this->session->user['id_organizacao']],
-            -1,
-            -1
-        );
-        $servicos = $this->servico->get_all(
-            '*',
-            ['situacoes.organizacao_fk' => $this->session->user['id_organizacao']],
-            -1,
-            -1,
-            [
-                ['table' => 'situacoes', 'on' => 'situacoes.situacao_pk = servicos.situacao_padrao_fk']
-            ]
-        );
-        $departamentos = $this->departamento->get_all(
-            '*',
-            ['organizacao_fk' => $this->session->user['id_organizacao']],
-            -1,
-            -1
-        );
-        $tipos_servicos = $this->tipo_servico->get_all(
-            '*',
-            ['departamentos.organizacao_fk' => $this->session->user['id_organizacao']],
-            -1,
-            -1,
-            [
-                ['table' => 'departamentos', 'on' => 'departamentos.departamento_pk = tipos_servicos.departamento_fk']
-            ]
-        );
-        $this->session->set_flashdata('css', [
-            0 => base_url('assets/css/modal_desativar.css'),
-            1 => base_url('assets/vendor/bootstrap-multistep-form/bootstrap.multistep.css'),
-            2 => base_url('assets/css/loading_input.css'),
-            3 => base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css'),
-            4 => base_url('assets/css/modal_map.css'),
-            5 => base_url('assets/css/timeline.css'),
-            6 => base_url('assets/css/style_card.css'),
-            7 => base_url('assets/css/user_guide.css'),
-        ]);
-        $this->session->set_flashdata('scripts', [
-            0 => base_url('assets/vendor/masks/jquery.mask.min.js'),
-            1 => base_url('assets/vendor/bootstrap-multistep-form/bootstrap.multistep.js'),
-            2 => base_url('assets/js/masks.js'),
-            3 => base_url('assets/vendor/bootstrap-multistep-form/jquery.easing.min.js'),
-            4 => base_url('assets/vendor/datatables/datatables.min.js'),
-            5 => base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js'),
-            6 => base_url('assets/js/utils.js'),
-            7 => base_url('assets/js/constants.js'),
-            8 => base_url('assets/js/jquery.noty.packaged.min.js'),
-            9 => base_url('assets/js/dashboard/mapa/mapa.js'),
-            10 => base_url('assets/vendor/select-input/select-input.js'),
-            11 => base_url('assets/js/localizacao.js')
-        ]);
-        $this->session->set_flashdata('mapa', [
-            0 => true,
-        ]);
-        load_view([
-            0 => [
-                'src' => 'dashboard/administrador/mapa/home',
-                'params' => [
-                    'prioridades' => $prioridades,
-                    'situacoes' => $situacoes,
-                    'servicos' => $servicos,
-                    'departamentos' => $departamentos,
-                    'tipos_servicos' => $tipos_servicos,
-                ],
-            ],
-            1 => [
-                'src' => 'access/pre_loader',
-                'params' => null,
-            ],
-        ], 'administrador');
-    }
-
     public function novo_relatorio()
     {
         //Carregando os models para recuperação de dados a serem exibidos na view Novo Relatório
@@ -180,6 +91,7 @@ class Relatorio extends CRUD_Controller
         $this->load->model('Tipo_Servico_model', 'tipo_servico');
         $this->load->model('Setor_model', 'setor');
         $this->load->helper('form');
+        
         $prioridades = $this->prioridade->get_all(
             '*',
             ['organizacao_fk' => $this->session->user['id_organizacao']],
