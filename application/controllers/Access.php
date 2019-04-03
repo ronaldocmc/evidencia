54661bbf27bb06a185cdfa7355db4c699cc47492
Robson Cruz <ghost0092@gmail.com>
2019-04-03 13:23:13 -0300

    fix: Fixed slashes and bad casing

--- application/controllers/Access.php
+++ application/controllers/Access.php
@@ -1,7 +1,7 @@
 <?php 
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 require_once(dirname(__FILE__)."/Response.php");	
-require_once APPPATH . "core\MyException.php";
+require_once APPPATH . "core/MyException.php";
 /**
  * Acess Class
  *
