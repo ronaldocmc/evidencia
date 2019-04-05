969592d236f179c0689de5a10691686537dc57ad
= <darlannakamura@hotmail.com>
2019-04-04 23:59:13 -0300

    authentication controller que será a classe mais abstrata com o método is_authorized

---
+++ application/core/AuthenticationController.php
@@ -0,0 +1,47 @@
+
+<?php
+
+/**
+ * CRUD_Controller
+ *
+ * @package     application
+ * @subpackage  core
+ * @author      Darlan
+ */
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+require_once APPPATH."core/Response.php";
+require_once APPPATH."core/MyException.php";
+
+class AuthenticationController extends CI_Controller
+{
+    public function is_authorized()
+    {
+        $this->load->library('Authorization');
+
+        $this->authorization = new Authorization();
+
+        return $this->authorization->check_permission( 
+            $this->get_current_controller(), 
+            $this->get_current_method()
+        );
+    }
+
+    private function get_current_controller()
+    {
+        return strtolower($this->uri->segment(1));
+    }
+
+    private function get_current_method()
+    {
+        return strtolower($this->uri->segment(2));
+    }
+
+    public function is_superuser()
+    {
+        return $this->session->user['is_superusuario'];
+    }
+}
