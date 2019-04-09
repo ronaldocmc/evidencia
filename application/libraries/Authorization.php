d27395ad56e9af70f588689543bcf397882cebbf
Robson Cruz <ghost0092@gmail.com>
2019-04-09 09:41:42 -0300

    Added log message to the Authorization proccess

--- application/libraries/Authorization.php
+++ application/libraries/Authorization.php
@@ -1,18 +1,19 @@
-<?php 
+<?php
 
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-require_once APPPATH."core/Response.php";
+require_once APPPATH . "core/Response.php";
 
-class Authorization {
+class Authorization
+{
 
     protected $CI;
 
     public function __construct()
     {
-        $this->CI =& get_instance();
+        $this->CI = &get_instance();
         $this->CI->load->model('Permissao_model', 'model');
     }
 
@@ -37,7 +38,7 @@ class Authorization {
     public function return_permissions()
     {
         $this->_check_if_has_user();
-        
+
         $function_id = $this->CI->session->user['funcao_id'];
 
         return $this->_get_permissions($function_id);
@@ -49,44 +50,43 @@ class Authorization {
 
         $function_id = $this->CI->session->user['id_funcao'];
 
-        if(!$this->_is_empty($controller) && !$this->_is_empty($method))
-        {
-            //$authorized = $this->check_permission_on_memory($function_id, $controller, $method);
-            $authorized = $this->CI->model->check_permission($function_id, $controller, $method);
-            
-            return $authorized;
-        } 
-        else 
-        {
-            echo 'OUTRAS REGRAS'; die();
-            //TODO - other rules
-        }
+        log_message('monitoring', 'Checking if ' . strtoupper($this->get_current_method()) . ' is possible in ' . strtoupper($this->get_current_controller()) . ' for ' . $this->get_current_user());
+        if (!$this->_is_empty($controller) && !$this->_is_empty($method)) {
+                $authorized = $this->check_permission_on_memory($function_id, $controller, $method);
+                //$authorized = $this->CI->model->check_permission($function_id, $controller, $method);
+
+                return $authorized;
+            } else {
+                echo 'OUTRAS REGRAS';
+                die();
+                //TODO - other rules
+            }
     }
 
     private function check_permission_on_memory($function_id, $controller, $method)
     {
         $this->CI->load->driver('cache', array('adapter' => 'apc', 'backup' => 'file'));
-        
+
         $permissions = $this->CI->cache->get('permissions');
 
-        if(array_key_exists($function_id, $permissions)){
+        if (array_key_exists($function_id, $permissions)) {
 
             $permissions_of_function = $permissions[$function_id]['permissions'];
 
-            foreach($permissions_of_function as $p){
-                if(strtolower($p['controller']) == strtolower($controller) 
-                && strtolower($p['method']) == strtolower($method)){
+            foreach ($permissions_of_function as $p) {
+                if (
+                    strtolower($p['controller']) == strtolower($controller)
+                    && strtolower($p['method']) == strtolower($method)
+                ) {
                     return true;
                 }
             }
 
             return false;
         } else {
-            echo 'caiu aqui'; 
+            echo 'caiu aqui';
             return false;
         }
-
-
     }
 
     private function _has_user()
@@ -96,11 +96,12 @@ class Authorization {
 
     private function _check_if_has_user()
     {
-        if(!$this->_has_user())
-        {
-            throw new MyException('É necessário que o usuário esteja logado.', 
-            Response::UNAUTHORIZED);
-        }
+        if (!$this->_has_user()) {
+                throw new MyException(
+                    'É necessário que o usuário esteja logado.',
+                    Response::UNAUTHORIZED
+                );
+            }
     }
 
     private function _is_empty($data)
@@ -114,28 +115,26 @@ class Authorization {
 
         $permissions = $this->CI->model->get_all_permissions();
 
-        foreach($permissions as $p)
-        {
-            if(array_key_exists($p->funcao_pk, $memory_permissions)){
+        foreach ($permissions as $p) {
+                if (array_key_exists($p->funcao_pk, $memory_permissions)) {
 
-                $permission = [
-                    'action'        => $p->acao_nome,
-                    'method'        => $p->acao_metodo,
-                    'entity'        => $p->entidade,
-                    'controller'    => $p->controller,
-                    'id'            => $p->permissao_pk
-                ];
+                    $permission = [
+                        'action'        => $p->acao_nome,
+                        'method'        => $p->acao_metodo,
+                        'entity'        => $p->entidade,
+                        'controller'    => $p->controller,
+                        'id'            => $p->permissao_pk
+                    ];
 
-                // $memory_permissions[$p->funcao_pk]['permissions'][] = $permission;
+                    // $memory_permissions[$p->funcao_pk]['permissions'][] = $permission;
 
-                array_push($memory_permissions[$p->funcao_pk]['permissions'], $permission);
-                
-            } else {
-                $memory_permissions[$p->funcao_pk]['function'] = ['name' => $p->funcao_nome];
+                    array_push($memory_permissions[$p->funcao_pk]['permissions'], $permission);
+                } else {
+                    $memory_permissions[$p->funcao_pk]['function'] = ['name' => $p->funcao_nome];
 
-                $memory_permissions[$p->funcao_pk]['permissions'] = [];
+                    $memory_permissions[$p->funcao_pk]['permissions'] = [];
+                }
             }
-        }
 
         return $memory_permissions;
     }
@@ -146,15 +145,13 @@ class Authorization {
 
         $permissions = $this->CI->model->get_permissions($function_id);
 
-        foreach($permissions as $p)
-        {
-            $data = [];
-            $data['name'] = $p->acao .' '.$p->entidade;
-            $data['id'] = $p->permissao_pk;
-            array_push($return_permissions, $data);
-        }
+        foreach ($permissions as $p) {
+                $data = [];
+                $data['name'] = $p->acao . ' ' . $p->entidade;
+                $data['id'] = $p->permissao_pk;
+                array_push($return_permissions, $data);
+            }
 
         return $return_permissions;
     }
 }
-?>
