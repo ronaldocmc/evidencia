4d96bc5a55bd20144f6ad078fe0d385fc1f4b5d5
Gustavo Gimenez de Deus <gustavogdd2@gmail.com>
2019-04-10 09:53:25 -0300

    Alterações
    Criação de um método para preencher dinâmicamente com todos os checkbox de permissões.
    Outro método checa os checkbox de acordo com os que a função já tem quando clica em editar.

--- assets/js/dashboard/funcao/index.js
+++ assets/js/dashboard/funcao/index.js
@@ -3,18 +3,52 @@
  * 
  * @Boolean: is_superusuario
  * 
- * 
  * @String: base_url
  * 
  */
 
 class View extends GenericView {
-    // FUNÇÃO GENÉRICA PARA PREENCHER UM SELECT
-    // PARA PREENCHER UM MULTIPLE SELECT
-
 
     constructor() {
         super();
+    }
+
+    async fillPermissions(permissions) {
+        let title;
+        let checkbox = '';
+        let entities = [];
+        let exists;
+        
+        permissions.forEach(permission => {
+            exists = false;
+
+            checkbox = this.generateCheckBox('acao_nome', 'permissao_pk', permission, 'permissoes');
+
+            entities.forEach(e => {
+                if (e === permission.entidade) {
+                    exists = true;
+                }
+            });
+
+            if (!exists) {
+                $('#permissions').append(`<div id='${permission.entidade.split(' ').join('_')}' class='conteiner'></div>`);
+                entities.push(permission.entidade);
+                title = this.generateTitle(permission.entidade, 4);
+                $(`#${permission.entidade.split(' ').join('_')}`).append(title + '<br>');
+            }
+
+            $(`#${permission.entidade.split(' ').join('_')}`).append(checkbox + '&nbsp' + '&nbsp');
+        });
+
+        entities.forEach(e => {
+            $(`#${e.split(' ').join('_')}`).append(`<br><hr>`);
+        });
+    }
+
+    checkPermissions(permissions) {
+        permissions.forEach(permission => {
+            $(`#id-${permission.id}`).prop('checked', true);
+        });
     }
 
 }
@@ -38,9 +72,41 @@ class Control extends GenericControl {
         this.tableFields = ['funcao_nome'];
         this.verifyDependences = true;
     }
+
+    async init() {
+        super.init();
+
+        let response = await this.myRequests.send('/get_all_permissions', {});
+        let permissions = response.data.permissions;
+
+        this.myView.fillPermissions(permissions);
+    }
+
+    async handleFillFields() {
+        super.handleFillFields('edit');
+
+        const response = await this.myRequests.send('/get_all_permissions',
+            {
+                funcao: this.data.self[this.state.selectedId][this.primaryKey]
+            });
+
+        this.myView.checkPermissions(response.data.permissions);
+    }
+
+    save () {
+        let permissions = [];
+        const data = {};
+        $.each($('input[name="permissoes"]:checked'), function () {
+            let id = $(this).attr('id');
+            id = id.split('-');
+            permissions.push(id[1]);
+        });
+
+        data.permissions = permissions;
+        super.save(data);
+    }
 }
 
 const myControl = new Control();
 
 myControl.init();
-
