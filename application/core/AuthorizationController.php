4b1ed20b347396d8750af0cec9b341883f7deeec
Gustavo Gimenez de Deus <gustavogdd2@gmail.com>
2019-04-18 10:20:33 -0300

    Passagem dos arrays e dos métodos de auth do CRUD para cá

--- application/core/AuthorizationController.php
+++ application/core/AuthorizationController.php
@@ -18,16 +18,34 @@ require_once APPPATH . "core/MyException.php";
 
 class AuthorizationController extends CI_Controller
 {
+
+    /**
+    * Authorization arrays
+    */
+    private $standart_methods = ['save', 'deactivate', 'activate', 'get'];
+    private $controllers_methods = [
+        'Funcionario' => ['change_password'],
+        'Ordem_Servico' => ['insert_situacao', 'delete', 'get_map'],
+        'Relatorio' => ['create_new_report', 'change_worker', 'receive_report']
+    ];
+
     public function is_authorized()
     {
         $this->load->library('Authorization');
 
         $this->authorization = new Authorization();
 
-        return $this->authorization->check_permission(
-            $this->get_current_controller(),
-            $this->get_current_method()
-        );
+        if ($this->verify_standart_methods() || $this->verify_controllers_methods()) 
+        {
+            return $this->authorization->check_permission(
+                $this->get_current_controller(),
+                $this->get_current_method()
+            );
+        }
+        else 
+        {
+            return true;
+        }
     }
 
     protected function get_current_controller()
@@ -44,4 +62,35 @@ class AuthorizationController extends CI_Controller
     {
         return $this->session->user['is_superusuario'];
     }
+
+    private function verify_standart_methods()
+    {
+        $method = $this->get_current_method();
+
+        if (in_array($method, $this->standart_methods)) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private function verify_controllers_methods()
+    {
+        $controller = $this->get_current_controller();
+        $method = $this->get_current_method();
+
+        if (array_key_exists($controller, $this->controllers_methods)) 
+        {
+            if (in_array($method, $this->controllers_methods[$controller])) 
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
