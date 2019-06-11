e35a5eb61812f8d49667d229d52827ade72ced8b
Gustavo <gustavogdd2@gmail.com>
2019-06-11 11:31:21 -0300

    feature: feature's logic

--- assets/js/dashboard/organizacao/edit_info.js
+++ assets/js/dashboard/organizacao/edit_info.js
@@ -5,6 +5,7 @@ class View extends GenericView {
     }
 
     init(data, tableFields, primaryKey) {
+        this.state.data = data;
         this.state.tableFields = tableFields;
         this.state.self = data.self;
         this.primaryKey = primaryKey;
@@ -24,6 +25,16 @@ class View extends GenericView {
         });
 
         this.generateSelect(data.municipios, 'municipio_nome', 'municipio_pk', 'localizacao_municipio');
+
+        data.self_municipios.forEach( (municipio, pos) => {
+            this.generateCity(municipio, pos);
+        });
+    }
+
+    generateCity(municipio, pos) {
+        $('#cities').append(`<div id="${municipio.municipio_nome.split(' ').join('_')}"> </div>`);
+        $(`#${municipio.municipio_nome.split(' ').join('_')}`).append(this.generateParagraph(municipio.municipio_nome));
+        $(`#${municipio.municipio_nome.split(' ').join('_')}`).append(this.createButton('remove', municipio.municipio_pk, 'danger', 'remover', pos, 'fa-times'));
     }
 
 }
@@ -46,10 +57,13 @@ class Control extends GenericControl {
         this.tableFields = ['organizacao_pk', 'organizacao_nome', 'organizacao_cnpj', 'localizacao_rua', 'localizacao_num', 'localizacao_bairro'];
         this.fields = ['organizacao_pk', 'organizacao_nome', 'organizacao_cnpj', 'localizacao_rua', 'localizacao_num', 'localizacao_bairro', 'localizacao_municipio'];
         this.verifyDependences = false;
+        this.municipios_nome = null;
     }
 
-    init() {
-        super.init();
+    async init() {
+        await super.init();
+
+        this.municipios_nome = await this.getMunicipiosNome();
 
         // clicks de cidade
         $(document).on("click", "#btn-edit", () => {
@@ -64,6 +78,27 @@ class Control extends GenericControl {
             this.save();
         });
 
+        $(document).on("click", "#add_city", () => {
+            this.addCity();
+        });
+
+        $(document).on("click", ".btn_remove", (e) => {
+            this.removeCity(e.currentTarget);
+        });
+
+        $('#new_city').autocomplete({
+            source: this.municipios_nome
+        });
+    }
+
+    async getMunicipiosNome() {
+        let names = [];
+
+        await this.myView.state.data.municipios.forEach( municipio => {
+            names.push(municipio.municipio_nome);
+        });
+
+        return names;
     }
 
     async save() {
@@ -79,6 +114,86 @@ class Control extends GenericControl {
         delete sendData["senha"];
 
         this.myView.endLoad();
+    }
+
+    async addCity() {
+        let city = null;
+        let position;
+        let used = false;
+
+        await this.myView.state.data.municipios.forEach( (municipio, pos) => {
+            if (municipio.municipio_nome === $('#new_city').val()) {
+                city = municipio;
+                position = pos;
+
+                return;
+            }
+        });
+
+        if (city === null) {
+            this.myView.showMessage('failed', 'Erro', 'Cidade não encontrada');
+            return;
+        } else {
+            
+            await this.myView.state.data.self_municipios.forEach( municipio => {
+                if (municipio.municipio_pk === city.municipio_pk) {
+                    used = true;
+                    return;
+                }
+            });
+
+            if (used) {
+                this.myView.showMessage('failed', 'Erro', 'Cidade já adicionada');
+                return;
+            }
+        }
+        
+        let sendData = {municipio_fk: city.municipio_pk};
+        let response = await this.myRequests.send('/add_city', sendData);
+
+        this.handleReponse(response, 'add', city, position);
+    }
+
+    async removeCity(button) {
+        let position = button.value;
+        let city = this.myView.state.data.self_municipios[position];
+        let sendData = {municipio_fk: city.municipio_pk}        
+
+        let response = await this.myRequests.send('/remove_city', sendData);
+        
+        this.handleReponse(response, 'remove', city, position);
+    }
+
+    handleReponse(response, action, city, position) {
+        if (!response) {
+            this.myView.showMessage("failed", "Falha", "Entre em contato com a central!");
+            return;
+        }
+
+        if (response.code == 200) {
+            this.myView.showMessage("success", "Sucesso", "Operação realizada!");
+
+            this.doAction(action, city, position);
+        } else {
+            this.myView.showMessage("failed", "Falha", response.data.mensagem);
+        }
+    }
+
+    doAction(action, city, position) {
+        switch(action) {
+            case 'add':
+                this.myView.generateCity(city, position);
+                this.myView.state.data.self_municipios.push(city);
+                break;
+
+            case 'remove':
+                $(`#${city.municipio_nome.split(' ').join('_')}`).remove();
+
+                this.myView.state.data.self_municipios.splice(position, 1);
+                break;
+        }
+
+        $('#new_city').val('');
     }
 }
 
